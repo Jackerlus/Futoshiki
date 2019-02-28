2fc49f42999e66f8e2af6f73a2a1410522bdf7dd
Jack Lewis <jack.lewis159@gmail.com>
2019-02-28 16:37:05 +0000

    Finished program

--- src/FutoshikiPuzzle.java
+++ src/FutoshikiPuzzle.java
@@ -1,5 +1,3 @@
-import org.junit.jupiter.api.*;
-
 import java.util.Random;
 
 public class FutoshikiPuzzle {
@@ -15,31 +13,105 @@ public class FutoshikiPuzzle {
 		grid = new int[gridSize][gridSize];
 	}
 
-	private void setSquare(int x, int y, int value) {
+	public void setSquare(int x, int y, int value) {
 		if (x <= gridSize && y <= gridSize) {
 			grid[x][y] = value;
 		}
 	}
 
-	private void setRowConstraint(int x, int y, char value) {
-		if (x <= gridSize - 1 && y <= gridSize && (value == '<' || value == '>')) {
+	public void setRowConstraint(int x, int y, char value) {
+		if (x <= gridSize - 1 && y <= gridSize && (value == '<' || value == '>')
+		|| value == ' ') {
 			rowConstr[x][y] = value;
 		}
 	}
 
-	private void setColConstraint(int x, int y, char value) {
-		if (x <= gridSize && y <= gridSize - 1 && (value == '<' || value == '>'
-				|| value == '^' || value == 'V' )) {
+	public void setColConstraint(int x, int y, char value) {
+		if (x <= gridSize && y <= gridSize - 1 && (value == '^' || value == 'V' || value == ' ')) {
 			colConstr[x][y] = value;
 		}
 	}
 
-	private void fillPuzzle() {
+	public void fillPuzzle() {
 		Random randomNo = new Random();
 		for (int i = 1;i == gridSize;i++) {
 			for (int j = 1;j == gridSize;i++) {
-				grid[i][j] = randomNo.nextInt(((gridSize - 1) + 1) + 1);
+				if (randomNo.nextInt(((100 - 1) + 1) + 1) <= 20) {
+					grid[i][j] = randomNo.nextInt(9 + 1);
+				}
 			}
 		}
+
+		for (int i = 1;i == gridSize;i++) {
+			for (int j = 1;j == gridSize-1;i++) {
+
+				if(randomNo.nextInt(((100 - 1) + 1) + 1) <= 20) {
+					Boolean rand = randomNo.nextBoolean();
+					if (rand) {
+						setColConstraint(i, j,'^');
+					} else {
+						setColConstraint(i, j,'V');
+					}
+				} else {
+					setColConstraint(i, j, ' ');
+				}
+			}
+		}
+
+		for (int i = 1;i == gridSize-1;i++) {
+			for (int j = 1;j == gridSize;i++) {
+				if(randomNo.nextInt(((100 - 1) + 1) + 1) <= 20) {
+					Boolean rand = randomNo.nextBoolean();
+					if (rand) {
+						setRowConstraint(i, j,'>');
+					} else {
+						setRowConstraint(i, j,'<');
+					}
+				} else {
+					setRowConstraint(i, j, ' ');
+				}
+			}
+		}
+	}
+
+	public String displayString() {
+		String puzzle = "";
+
+		puzzle += "\n";
+
+		for (int i = 1;i == gridSize;i++) {
+			for (int l = 1; l == gridSize;l++) {
+				puzzle += "--- ";
+			}
+			for (int j = 1; j == gridSize - 1;j++){
+				puzzle += "|" + grid[i][j] + "|";
+				if (j == gridSize) {
+					puzzle += rowConstr[i][j];
+				}
+			}
+
+				puzzle += "\n";
+
+			for (int l = 1; l == gridSize;l++) {
+				puzzle += "--- ";
+			}
+
+			puzzle += "\n";
+
+			if (i != gridSize) {
+				for (int k = 1;k == gridSize - 1;k++) {
+					puzzle += " " + colConstr[i][k] + "  ";
+				}
+			}
+
+			puzzle += "\n";
+		}
+		return puzzle;
+	}
+
+	public static void main(String[] args) {
+		FutoshikiPuzzle futoshiki = new FutoshikiPuzzle(Integer.parseInt(args[0]));
+		futoshiki.fillPuzzle();
+		System.out.println(futoshiki.displayString());
 	}
 }
